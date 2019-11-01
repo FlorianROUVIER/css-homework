cac76ac4429f138b298197c18a3504fca24a1a80
FlorianRouvier <florian.rouvier@hetic.net>
2019-11-01 17:24:03 +0100

    Update v4

--- css-animation/script.js
+++ css-animation/script.js
@@ -9,3 +9,34 @@ menuButtonElement.addEventListener(
     }
 
 )
+
+
+
+const speedChange = document.querySelector('.speedCase-1')
+const propellers = document.querySelector('.js-propellers')
+const speedChange2 = document.querySelector('.speedCase-2')
+const speedChange3 = document.querySelector('.speedCase-3')
+
+speedChange.addEventListener(
+    'click',
+    ()=>
+    {
+        propellers.style.animation = "animation 9s linear infinite"
+    }
+)
+
+speedChange2.addEventListener(
+    'click',
+    ()=>
+    {
+        propellers.style.animation = "animation 2.5s linear infinite"
+    }
+)
+
+speedChange3.addEventListener(
+    'click',
+    () =>
+    {
+        propellers.style.animation = "animation 1.5s linear infinite"
+    }
+)
